9a9c4ae288aa60911f0eda8397f5f7bae5ce6774
dpmptspnakeragam <dpmptspnakeragam@gmail.com>
2020-07-29 11:28:50 +0700

    Update tampilan home admin

--- application/views/admin/home.php
+++ application/views/admin/home.php
@@ -1,6 +1,5 @@
 <main role="main" class="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4">
-<div class="container-fluid" 
-	style="background-image: url('<?= base_url(); ?>assets/img/bg2.jpg');
+	<div class="container-fluid shadow" style="background-image: url('<?= base_url(); ?>assets/img/wallpaperenaker.jpg');
 	background-attachment: fixed;
   	background-repeat: no-repeat;
   	background-size: cover;
@@ -8,11 +7,11 @@
   	padding: 100px 0;
   	height:80%;
   	background-color: black;
-  	opacity:0.9;" >
-	  <div class="text-center text-white">
-	    <h1>SELAMAT DATANG</h1>
-	    <img src="<?= base_url(); ?>assets/img/logoenaker(new).png" width="200px"  alt="Logo e-Naker" style="margin:20px; width:150px;">
-	    <h4>DINAS PENANAMAN MODAL PELAYANAN TERPADU SATU PINTU DAN KETENAGAKERJAAN<br/>KABUPATEN AGAM</h4>
-	  </div>
-</div>
+  	opacity:0.9;">
+		<div class="text-center text-white">
+			<h1 class="display-4 ">SELAMAT DATANG</h1>
+			<img src="<?= base_url(); ?>assets/img/logoenaker(new).png" alt="Logo e-Naker" style="margin:20px; width:130px;">
+			<h4 class="display-1 judul-admin">DINAS PENANAMAN MODAL PELAYANAN TERPADU SATU PINTU DAN KETENAGAKERJAAN<br />KABUPATEN AGAM</h4>
+		</div>
+	</div>
 </main>
