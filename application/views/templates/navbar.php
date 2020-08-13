b1f9c5d9a69d287826b7fd65ed364dac1d8e2e6b
dpmptspnakeragam <dpmptspnakeragam@gmail.com>
2020-08-13 13:52:03 +0700

    Hapus menu

--- application/views/templates/navbar.php
+++ application/views/templates/navbar.php
@@ -1,20 +1,20 @@
 <div class="text-white" style="background: linear-gradient(#343A40, #17A2B8);">
   <div class="container" style="padding-top:20px; padding-bottom:20px;">
     <div class="row">
-    <div class="col-lg-1 col-md-1 col-sm-1 text-center">
-      <img src="<?= base_url(); ?>assets/img/logoagam.png" width="20px" alt="Logo Agam">
-    </div>
-        <div class="col-lg-11 col-md-11 col-sm-11">
-      <h6>DINAS PENANAMAN MODAL PELAYANAN TERPADU SATU PINTU DAN KETENAGAKERJAAN KABUPATEN AGAM</h6>
+      <div class="col-lg-1 col-md-1 col-sm-1 text-center">
+        <img src="<?= base_url(); ?>assets/img/logoagam.png" width="20px" alt="Logo Agam">
+      </div>
+      <div class="col-lg-11 col-md-11 col-sm-11">
+        <h6>DINAS PENANAMAN MODAL PELAYANAN TERPADU SATU PINTU DAN KETENAGAKERJAAN KABUPATEN AGAM</h6>
+      </div>
     </div>
   </div>
 </div>
-</div>
 <nav class="navbar sticky-top navbar-expand-lg navbar-dark bg-info shadow">
-  <div class="container" >
+  <div class="container">
     <a class="navbar-brand" href="<?= base_url(); ?>">
       <img src="<?= base_url(); ?>assets/img/logoenaker(new)putih.png" width="50px" alt="Logo Naker" style="height: 30px; width: 30px;">
-     <span class="navbar-brand mb-0 h1">e-Naker</span>
+      <span class="navbar-brand mb-0 h1">e-Naker</span>
     </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarenaker" aria-controls="navbarenaker" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -51,10 +51,10 @@
             <a class="dropdown-item" href="<?= base_url(); ?>pemagangan">Pemagangan</a>
             <a class="dropdown-item" href="<?= base_url(); ?>sarana_hi">Sarana HI</a>
             <a class="dropdown-item dropdown-toggle" href="#" id="navbarDropdown2" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Lembaga Pelatihan Kerja Swasta</a>
-              <div class="dropdown-menu" aria-labelledby="navbarDropdown2">
-                <a class="dropdown-item" href="<?= base_url(); ?>profil_lpks">Profil</a>
-                <a class="dropdown-item" href="<?= base_url(); ?>alumni_lpks">Data Alumni</a>
-              </div>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown2">
+              <a class="dropdown-item" href="<?= base_url(); ?>profil_lpks">Profil</a>
+              <a class="dropdown-item" href="<?= base_url(); ?>alumni_lpks">Data Alumni</a>
+            </div>
           </div>
         </li>
         <li class="nav-item dropdown">
@@ -73,11 +73,11 @@
           </a>
           <div class="dropdown-menu" aria-labelledby="navbarDropdown">
             <a class="dropdown-item" href="<?= base_url(); ?>pencaker">Data Pencari Kerja</a>
-            <a class="dropdown-item" href="<?= base_url(); ?>bursa_kerja">Jobfair / Bursa Kerja</a>
-            <a class="dropdown-item" href="<?= base_url(); ?>bursa_khusus">Bursa Kerja Khusus</a>
+            <!---<a class="dropdown-item" href="<?= base_url(); ?>bursa_kerja">Jobfair / Bursa Kerja</a>
+            <a class="dropdown-item" href="<?= base_url(); ?>bursa_khusus">Bursa Kerja Khusus</a>--->
           </div>
         </li>
       </ul>
-   </div>
- </div>
-</nav> 
+    </div>
+  </div>
+</nav>
