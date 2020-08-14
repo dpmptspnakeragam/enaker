7dfa05a40286bd830722ac6f74686300605e1678
dpmptspnakeragam <dpmptspnakeragam@gmail.com>
2020-08-14 14:31:18 +0700

    Tambah Security Login Captcha Penjumlahan

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -19,55 +19,62 @@ class Login extends CI_controller
 	{
 		$this->form_validation->set_rules('usrname', 'Username', 'required');
 		$this->form_validation->set_rules('pssword', 'Password', 'required');
-		if ($this->form_validation->run()  == FALSE) {
-			$this->load->view('templates/header_admin');
-			$this->load->view('login');
-			$this->load->view('templates/footer_admin');
-		} else {
-			$data = array(
-				'username' => $this->input->post('usrname', TRUE),
+		$angka = $this->input->post('angka1', TRUE) + $this->input->post('angka2', TRUE);
+		$jumlah = $this->input->post('c', TRUE);
 
-				'pass' => $this->input->post('pssword', TRUE)
-			);
-
-			$this->load->model('Model_user'); // load model_user
-			$hasil = $this->Model_user->cek_user($data);
-
-			if ($hasil->num_rows() == 1) {
-				foreach ($hasil->result() as $sess) {
-					$sess_data['logged_in'] = TRUE;
-					$sess_data['hak_akses'] = $sess->hak_akses;
-					$sess_data['nama'] = $sess->nama;
-					$sess_data['username'] = $sess->username;
-					$sess_data['id_nagari2'] = $sess->id_nagari2;
-					$sess_data['id_nagari'] = $sess->id_nagari;
-					$sess_data['id_sekolah'] = $sess->id_sekolah;
-					$sess_data['id_perusahaan'] = $sess->id_perusahaan;
-
-					$this->session->set_userdata($sess_data);
-				}
-				if ($this->session->userdata('hak_akses') == 'superadmin') {
-					redirect('admin/home');
-				} elseif ($this->session->userdata('hak_akses') == 'upt') {
-					redirect('admin/home');
-				} elseif ($this->session->userdata('hak_akses') == 'hi') {
-					redirect('admin/home');
-				} elseif ($this->session->userdata('hak_akses') == 'p2k2') {
-					redirect('admin/home');
-				} elseif ($this->session->userdata('hak_akses') == 'nagari') {
-					redirect('admin/home');
-				} elseif ($this->session->userdata('hak_akses') == 'kecamatan') {
-					redirect('admin/home');
-				} elseif ($this->session->userdata('hak_akses') == 'sekolah') {
-					redirect('admin/home');
-				} elseif ($this->session->userdata('hak_akses') == 'perusahaan') {
-					redirect('admin/home');
-				}
+		if ($angka == $jumlah) {
+			if ($this->form_validation->run()  == FALSE) {
+				$this->load->view('templates/header_admin');
+				$this->load->view('login');
+				$this->load->view('templates/footer_admin');
 			} else {
-				$this->session->set_flashdata('pesan', 'Maaf, Username atau Password anda <b>Salah</b>');
+				$data = array(
+					'username' => $this->input->post('usrname', TRUE),
 
-				redirect('login');
+					'pass' => $this->input->post('pssword', TRUE)
+				);
+
+				$this->load->model('Model_user'); // load model_user
+				$hasil = $this->Model_user->cek_user($data);
+
+				if ($hasil->num_rows() == 1) {
+					foreach ($hasil->result() as $sess) {
+						$sess_data['logged_in'] = TRUE;
+						$sess_data['hak_akses'] = $sess->hak_akses;
+						$sess_data['nama'] = $sess->nama;
+						$sess_data['username'] = $sess->username;
+						$sess_data['id_nagari2'] = $sess->id_nagari2;
+						$sess_data['id_nagari'] = $sess->id_nagari;
+						$sess_data['id_sekolah'] = $sess->id_sekolah;
+						$sess_data['id_perusahaan'] = $sess->id_perusahaan;
+
+						$this->session->set_userdata($sess_data);
+					}
+					if ($this->session->userdata('hak_akses') == 'superadmin') {
+						redirect('admin/home');
+					} elseif ($this->session->userdata('hak_akses') == 'upt') {
+						redirect('admin/home');
+					} elseif ($this->session->userdata('hak_akses') == 'hi') {
+						redirect('admin/home');
+					} elseif ($this->session->userdata('hak_akses') == 'p2k2') {
+						redirect('admin/home');
+					} elseif ($this->session->userdata('hak_akses') == 'nagari') {
+						redirect('admin/home');
+					} elseif ($this->session->userdata('hak_akses') == 'kecamatan') {
+						redirect('admin/home');
+					} elseif ($this->session->userdata('hak_akses') == 'sekolah') {
+						redirect('admin/home');
+					} elseif ($this->session->userdata('hak_akses') == 'perusahaan') {
+						redirect('admin/home');
+					}
+				} else {
+					$this->session->set_flashdata('pesan', 'Maaf, Username atau Password anda <b>Salah</b>');
+					redirect('login');
+				}
 			}
+		} else {
+			$this->session->set_flashdata('pesan', 'Maaf, Penjumlahan <b>Salah</b>');
+			redirect('login');
 		}
 	}
 }
--- application/views/login.php
+++ application/views/login.php
@@ -1,33 +1,47 @@
 <body style="background: linear-gradient(#343A40, #17A2B8);">
     <div class="container-fluid">
-      <div class="row">
-	    <div class="col-lg-12 col-sm-12 text-center text-white" style="margin:80px auto;">
-	    <img src="<?= base_url(); ?>assets/img/logoenaker(new)putih.png" width="110px" alt="Logo e-Naker">
-	    <h1>e-Naker</h1>
-	    <h6>DINAS PENANAMAN MODAL PELAYANAN TERPADU SATU PINTU DAN KETENAGAKERJAAN <br> KABUPATEN AGAM</h6>
-	    <h2>Silahkan Login</h2>
-	    <div class="card-body" style="width:400px; margin:auto;">
-            <?php if( $this->session->flashdata('pesan')) : ?>
-                <div class="alert alert-danger" role="alert">
-                     <?= $this->session->flashdata('pesan'); ?>
+        <div class="row">
+            <div class="col-lg-12 col-sm-12 text-center text-white" style="margin:40px auto;">
+                <img src="<?= base_url(); ?>assets/img/logoenaker(new)putih.png" width="80px" alt="Logo e-Naker">
+                <h1>e-Naker</h1>
+                <h6>DINAS PENANAMAN MODAL PELAYANAN TERPADU SATU PINTU DAN KETENAGAKERJAAN <br> KABUPATEN AGAM</h6>
+                <h3>Silahkan Login</h3>
+                <div class="card-body" style="width:400px; margin:auto;">
+                    <?php if ($this->session->flashdata('pesan')) : ?>
+                        <div class="alert alert-danger" role="alert">
+                            <?= $this->session->flashdata('pesan'); ?>
+                        </div>
+                    <?php endif; ?>
+                    <form role="form" action="<?= base_url(); ?>login/cek_login" method="post">
+                        <fieldset>
+                            <div class="form-group">
+                                <input class="form-control shadow" autocomplete="off" placeholder="Masukkan Username" name="usrname" autofocus>
+                                <small id="emailHelp" class="form-text text-danger"><?= form_error('usrname'); ?></small>
+                            </div>
+                            <div class="form-group">
+                                <input class="form-control shadow" autocomplete="off" placeholder="Masukkan Password" name="pssword" type="password">
+                                <small id="emailHelp" class="form-text text-danger"><?= form_error('pssword'); ?></small>
+                            </div>
+                            <div class="form-group">
+                                <p>Berapakah jumlah</p>
+                                <div class="row">
+                                    <div class="col">
+                                        <input class="form-control" type='number' value='<?= rand(1, 10) ?>' name='angka1' autocomplete=off readonly>
+                                    </div>+
+                                    <div class="col">
+                                        <input class="form-control" type='number' value='<?= rand(1, 10) ?>' name='angka2' autocomplete=off readonly>
+                                    </div>=
+                                    <div class="col">
+                                        <input class="form-control" type='number' name='c' placeholder='Jawaban' autocomplete=off>
+                                    </div>
+                                </div>
+                                <hr>
+                                <!-- Change this to a button or input when using this as a form -->
+                                <button type="submit" class="shadow btn btn-lg btn-danger btn-block">Login</button>
+                        </fieldset>
+                    </form>
                 </div>
-            <?php endif ;?>
-            <form role="form" action="<?= base_url();?>login/cek_login" method="post">
-                <fieldset>
-                    <div class="form-group">
-                        <input class="form-control shadow" autocomplete="off" placeholder="Masukkan Username" name="usrname" autofocus>
-                        <small id="emailHelp" class="form-text text-danger"><?= form_error('usrname'); ?></small>
-                    </div>
-                    <div class="form-group">
-                        <input class="form-control shadow" autocomplete="off" placeholder="Masukkan Password" name="pssword" type="password">
-                        <small id="emailHelp" class="form-text text-danger"><?= form_error('pssword'); ?></small>
-                    </div>
-                    <!-- Change this to a button or input when using this as a form -->
-                    <button type="submit" class="shadow btn btn-lg btn-danger btn-block">Login</button>
-                </fieldset>
-            </form>
-    	</div>
-	  </div>
-      </div>
+            </div>
+        </div>
     </div>
-  </body>
+</body>
